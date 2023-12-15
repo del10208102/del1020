b6f3294705747721290d143bf7c6d1a1039313e9
del10208102 <148189739+del10208102@users.noreply.github.com>
2023-12-15 10:30:45 +0800

    Update top.v

--- top.v
+++ top.v
@@ -5,6 +5,7 @@ module top_module(
   reg clk;
   reg rst_n;
   wire beep;
+  wire [639:0][479:0] screen; // 640x480 screen represented by a 2D array
 
   // Instantiate beep_music module
   beep_music beep_player(
@@ -22,9 +23,24 @@ module top_module(
     rst_n = 0;
     #5 rst_n = 1;  // Release reset after 5 time units
 
+    // Initialize screen to all zeros (black)
+    for (int i = 0; i < 640; i = i + 1) begin
+      for (int j = 0; j < 480; j = j + 1) begin
+        screen[i][j] = 0;
+      end
+    end
+
+    // Example: Draw a white rectangle on the screen
+    for (int i = 100; i < 200; i = i + 1) begin
+      for (int j = 100; j < 200; j = j + 1) begin
+        screen[i][j] = 1; // 1 represents white color
+      end
+    end
+
     // Provide a clock signal
     forever #5 clk = ~clk;  // Toggle the clock every 5 time units
   end
+end
 
   // Your simulation or synthesis code here
 
